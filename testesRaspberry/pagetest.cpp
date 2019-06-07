321a60b66df4d6f66c5bd92d2cd9a8ac33de0829
Gabriel Oliveira <gabriel.oliveira@engenharia.ufjf.br>
2019-06-07 18:24:53 -0300

    Adicionando o código para mudar página e imprimir o endereço selecionado

--- testesRaspberry/pagetest.cpp
+++ testesRaspberry/pagetest.cpp
@@ -9,17 +9,30 @@ short int invWord(short int  word);
 int main(){
 int fd1;
 short int result;
-int addr_zero = 0x00;
+int pageNumber = 2;
+int addrZero = 0x00;
+int addrPage = 0xF0;
 
 fd1 = wiringPiI2CSetup(0x5D);
 
 
 cout << "Init Result: " << fd1 << endl;
 
-cout << "Changing the register page" << endl;
+regPage = wiringPiI2CReadReg16(fd1, addrPage);
+regPage = invWord(regPage);
 
-result = wiringPiI2CReadReg16(fd1, addr_zero + i);
+cout << "Current register page: " dec << regPage << endl;
+cout << "Changing to register page " << pageNumber << endl;
+
+int wiringPiI2CWriteReg16 (fd1, addrPage, pageNumber);
+
+cout << "Reading from page " << dec << pageNumber << " address " << hex << addrZero << "should be 0x00" << endl; 
+
+result = wiringPiI2CReadReg16(fd1, addrZero);
 result = invWord(result);
+
+cout << "Value on address " << hex << addrZero << " = " << result << endl;
+
 
 }
 
