2eefc47fd11522dccb6abc8e30db08682a23eafe
Gabriel Oliveira <gabriel.ayresdeoliveira@mail.utoronto.ca>
2019-06-09 19:30:37 -0300

    Adicionando um script simples para iniciar o clock da câmera

---
+++ testesRaspberry/initClock.sh
@@ -0,0 +1,15 @@
+#Esse script inicia o clock no pino designado com a frequência designada
+
+pinoClock = 7
+frequencia = 4000000
+
+echo "Iniciando o clock"
+gpio mode $pinoClock clock
+
+echo "Selecionando a frequência de 4000000"
+gpio clock $pinoClock $frequencia
+
+echo "Rodando a ferramenta de I2C para testar"
+i2cdetect -y 1
+
+
