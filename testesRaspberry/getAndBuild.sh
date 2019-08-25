dc85e3bd2ffac269579e38eea7c1ecae03f1bfd7
Gabriel Oliveira <gabriel.ayresdeoliveira@mail.utoronto.ca>
2019-08-25 20:01:49 -0300

    git

---
+++ testesRaspberry/getAndBuild.sh
@@ -0,0 +1,10 @@
+if ["$1" == ""]; then
+    echo "Uso: getAndBuild entrada saida";
+    exit
+fi
+
+echo "Atualizando os arquivos\n"
+git pull
+echo "Compilando o arquivo\n" 
+
+g++ $1 -lwiringPi -o $2
