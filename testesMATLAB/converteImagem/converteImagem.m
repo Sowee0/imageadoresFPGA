7cdfac9efd5200aa45571cd56dac94b67b7ccc52
Gabriel Oliveira <gabriel.oliveira@engenharia.ufjf.br>
2019-10-30 19:44:10 -0300

    Criação de um código em matlab que lê uma imagem e transforma em memoria em verilog

---
+++ testesMATLAB/converteImagem/converteImagem.m
@@ -0,0 +1,28 @@
+clc
+close all
+
+contador = 0;
+valorPixel = 0;
+
+
+imagemLida = imread('converter.bmp');
+saida = fopen("saida.txt",'w');
+
+imagemCinza = rgb2gray (imagemLida);
+
+alturaImagem    = size(imagemCinza,1);
+larguraImagem   = size(imagemCinza,2);
+
+for a = 1:alturaImagem;  
+    for l = 1:larguraImagem;
+        
+        contadorBinario = dec2bin(contador ,8);
+        valorPixel = uint16((imagemCinza(a, l)))*4;
+        
+        texto = '\t\t8d''%d:\toVAL <= 10d''%d;\n';
+        fprintf(saida, texto, contador, valorPixel);
+        
+        
+        contador = contador + 1;
+    end
+end
