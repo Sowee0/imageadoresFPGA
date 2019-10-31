6cab0ea87ba5448d37ca37dc048ffd19f19d86bb
Gabriel Oliveira <gabriel.oliveira@engenharia.ufjf.br>
2019-10-30 21:13:57 -0300

    Corrigindo o tamanho das variáveis

--- testesFPGA/procuraPupila/V/COORD2ADDR.v
+++ testesFPGA/procuraPupila/V/COORD2ADDR.v
@@ -1,10 +1,10 @@
 module COORD2ADDR(
-			input [13:0]	iX,
-			input [13:0]	iY,
-			output[13:0]	oAddr
+			input [12:0]	iX,
+			input [12:0]	iY,
+			output[17:0]	oAddr
 			);
 			
-parameter [10:0] offsetY = 10'd512;
+parameter [17:0] offsetY = 10'd512;
 			
 always @ (iX or iY) begin
 
