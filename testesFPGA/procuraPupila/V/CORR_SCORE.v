d0f56bdbb6d26dbb080e520b5c0b7e0049326f8c
Gabriel Oliveira <gabriel.oliveira@engenharia.ufjf.br>
2019-10-30 21:13:38 -0300

    Adicionando uma logica de avaliação de correlação

--- testesFPGA/procuraPupila/V/CORR_SCORE.v
+++ testesFPGA/procuraPupila/V/CORR_SCORE.v
@@ -12,11 +12,72 @@ module CORR_SCORE(
 );
 			
 
+reg [15:0] endScore;
+reg [15:0] currentScore;
+reg [12:0] Xcoord;
+reg [12:0] Ycoord;
 
-always @ (posedge iCLK) begin
+assign oX_sram = iXstart + Xcoord;
+assign oY_sram = iYstart + Ycoord;
+
+assign oX_search = Xcoord;
+assign oY_search = Ycoord;
+
+
+always @ (iXstart or iYstart) begin
+
+Xcoord 			<= 0;
+Ycoord 			<= 0;
+finished 		<= 0;
+currentScore 	<= 0;
+endsScore		<= 0;
+
+end
+
+
+always @ (posedge iCLK and finished = 0) begin
+
+
+if(reading_sram > reading_search) begin
+
+currentScore <= 1023 - (reading_sram - reading_search);
+
+end
+
+if(reading_sram > reading_search) begin
+
+currentScore <= 1023 - (reading_search - reading_sram);
+
+end
+
+if(reading_sram == reading_search) begin
+
+currentScore <= 1023;
+
+end
+
+//daqui pra baixo tá tudo errado com a mais absoluta certeza.
+endScore = endScore + currentScore;
+
+if(Xcoord < 512 && Ycoord < 384)
+	Xcoord = Xcoord + 1;
+
+else if(Xcoord == 511 && Ycoord < 384) begin
+	Xcoord <= 0;
+	Ycoord = Ycoord + 1;
+	
+	end
+
+else if(Xcoord == 511 && Ycoord == 383) begin
+	finished = 1;
+
+	end
 
 
 
 
 
 end
+
+endmodule
+	
