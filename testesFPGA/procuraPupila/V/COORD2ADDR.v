4944f953f69e1e783585c5c74ed1ca47e23e1dde
Gabriel Oliveira <gabriel.ayresdeoliveira@mail.utoronto.ca>
2019-11-09 18:22:56 -0300

    Corrigindo a lista de sensibilidade e o tipo de variável

--- testesFPGA/procuraPupila/V/COORD2ADDR.v
+++ testesFPGA/procuraPupila/V/COORD2ADDR.v
@@ -1,15 +1,17 @@
 module COORD2ADDR(
 			input [12:0]	iX,
 			input [12:0]	iY,
-			output[17:0]	oAddr
+			output reg [17:0]	 oAddr
 			);
 			
 parameter [17:0] offsetY = 10'd512;
 			
-always @ (iX or iY) begin
+always @ (*) begin
 
 		
 		oAddr <= iX + iY * offsetY;
 	
 		
 end
+
+endmodule
