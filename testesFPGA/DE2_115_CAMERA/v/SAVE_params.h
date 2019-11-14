2c31847afca3b0c29a32760d2d771d969c951a75
Gabriel Oliveira <gabriel.oliveira@engenharia.ufjf.br>
2019-11-14 19:11:56 -0300

    Adicionando novos parametros da pesquisa

--- testesFPGA/DE2_115_CAMERA/v/SAVE_params.h
+++ testesFPGA/DE2_115_CAMERA/v/SAVE_params.h
@@ -1,4 +1,7 @@
 parameter H_START  	= 144;
 parameter V_START  	= 35;
 parameter H_RES		= 512;
-parameter V_RES		= 384;	
+parameter V_RES		= 384;
+
+parameter SEARCH_H_RES = 256;
+parameter SEARCH_V_RES = 256;
