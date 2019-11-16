76f84e4e48b982d24b442d32a0510f35730dcf63
Gabriel Oliveira <gabriel.ayresdeoliveira@mail.utoronto.ca>
2019-11-15 22:30:44 -0300

    corrigindo o tamanho da pesquisa

--- testesFPGA/DE2_115_CAMERA/v/SAVE_params.h
+++ testesFPGA/DE2_115_CAMERA/v/SAVE_params.h
@@ -3,5 +3,5 @@ parameter V_START  	= 35;
 parameter H_RES		= 512;
 parameter V_RES		= 384;
 
-parameter SEARCH_H_RES = 256;
-parameter SEARCH_V_RES = 256;
+parameter SEARCH_H_RES = 255;
+parameter SEARCH_V_RES = 255;
