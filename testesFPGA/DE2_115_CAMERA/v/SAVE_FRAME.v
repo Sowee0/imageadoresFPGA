b59810091de163cbfda14cb8b7b5d03ec523987e
Gabriel Oliveira <gabriel.oliveira@engenharia.ufjf.br>
2019-11-19 10:47:58 -0300

    Adicionando varias inicializaçoes e corrigindo tamanhos de registradores

--- testesFPGA/DE2_115_CAMERA/v/SAVE_FRAME.v
+++ testesFPGA/DE2_115_CAMERA/v/SAVE_FRAME.v
@@ -23,50 +23,59 @@ input	[12:0]	iX;
 input	[12:0]   iY;
 input 			iCLK;
 input				iSync;
-output			oReady;
-output			oStopCapture;
+output reg		oReady = 1'b0;
+output reg		oStopCapture;
 output reg		oLed;
-output			oMemData;
-output reg			oMemWE;
-output wire [14:0] oMemAddr;
+output [15:0]	oMemData;
+output reg		oMemWE;
+output wire [19:0] oMemAddr;
 
 
 reg [15:0] frameCounter = 16'd0;
-reg [14:0] memoryAddress = 0;
-reg [14:0] memoryData;
+reg [19:0] memoryAddress = 0;
+reg [15:0] memoryData;
 
 
+assign oMemData = memoryData;
 assign oMemAddr = memoryAddress;
 
 `include "SAVE_params.h"
 
 always @(posedge iCLK) begin
 
+		oStopCapture <= 0;
+
 
 		if(iSwitch && (frameCounter == 300)) begin
+		
+			oStopCapture <= 1;
 		
 			if(iX >= H_START && iX <= H_START + H_RES && iY >= V_START && iY <= V_START + V_RES) begin
 			
 			memoryAddress <= (iX - H_START) + (V_RES * (iY - V_START));
 			memoryData <= iRed;
 			oMemWE <= 1'b1;
+			
 			end	
 		
 		end
-		else
-		oMemWE <= 1'b0;
+		else begin
+			oMemWE <= 1'b0;
+			
+			if(frameCounter > 300)begin
+			oLed <= 1'b1;
+			oReady <= 1'b1;
+			end
+			
+			else
+			oLed <= 1'b0;
+		
+		end
 		
 		
 		//Choosing a recurring frame position as to count frames
 		if(iX == 143 && iY == 34)
 		frameCounter <= frameCounter + 1;
-	
-		if(frameCounter > 16'd300)
-		oLed <= 1'b1;
-	
-		else
-		oLed <= 1'b0;
-
 
 end
 
