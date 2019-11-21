ca9d8a3b366d0d106c7aabdadf02fe5ce8c27801
Gabriel Oliveira <gabriel.oliveira@engenharia.ufjf.br>
2019-11-21 19:09:10 -0300

    Corrigindo a logica

--- testesFPGA/DE2_115_CAMERA/v/CORR_SCORE.v
+++ testesFPGA/DE2_115_CAMERA/v/CORR_SCORE.v
@@ -22,7 +22,7 @@ output	[12:0] 	oY_sram;
 output	[12:0] 	oX_search;
 output 	[12:0]	oY_search;
 output   wire 		oFinished;
-output	reg [31:0] 	oScore;
+output	reg [31:0] 	oScore =0;
 
 
 //Including the file with various parameters
@@ -56,53 +56,48 @@ assign oY_search = Ycoord;
 
 always @ (posedge iCLK) begin
 
-	//Starting the correlation  process setting everything back to zero.
-	if(currentXcoord != iXstart && currentYcoord != iYstart) begin
-		Xcoord 			<= 0;
-		Ycoord 			<= 0;
-		finished 		<= 0;
-		currentScore 	<= 0;
-		endScore			<= 0;
-		currentXcoord 	<= iXstart;
-		currentYcoord	<= iYstart;
+	//
+	if(finished) begin
+		oScore <= endScore;
+		
+		if((iXstart != currentXcoord) && (iYstart != currentYcoord) ) begin
+			Xcoord 			<= 0;
+			Ycoord 			<= 0;
+			finished 		<= 0;
+			currentScore 	<= 0;
+			endScore			<= 0;
+			currentXcoord 	<= iXstart;
+			currentYcoord	<= iYstart;
+		end
 	end
 	
 	else begin
 	
-	
-
-	if(!finished) begin
-	
-		if(reading_sram > reading_search)
-		currentScore <= 16'd1023 - (reading_sram - reading_search);
+		if(reading_sram > reading_search)begin
+			currentScore <= 16'd1023 - (reading_sram - reading_search);
+			end
 		
-		else
-		if(reading_sram > reading_search)
-		currentScore <= 16'd1023 - (reading_search - reading_sram);
+		else if(reading_search > reading_sram)begin
+			currentScore <= 16'd1023 - (reading_search - reading_sram);
+			end
 		
-		else
-		if(reading_sram == reading_search)
-		currentScore <= 16'd1023;
+		else if(reading_sram == reading_search)begin
+			currentScore <= 16'd1023;
+			end
 		
 		endScore <= endScore + currentScore; 
-		
-	end
-	
-	else begin
-		oScore <= endScore;	
-	end
 	
 	
-	if(Xcoord < SEARCH_H_RES)
-		Xcoord <= Xcoord + 1'b1;
-	else if(Ycoord < SEARCH_V_RES) begin
-		Ycoord <= Ycoord + 1'b1;
-		Xcoord <= 0;
-	end 
-	else
-		finished <= 1'b1;
-	
-	end
+		if(Xcoord < SEARCH_H_RES2)
+			Xcoord <= Xcoord + 1'b1;
+		else if(Ycoord < SEARCH_V_RES2) begin
+			Ycoord <= Ycoord + 1'b1;
+			Xcoord <= 0;	
+		end 
+		else
+			finished <= 1'b1;
+		end
+			
 end
 
 endmodule
