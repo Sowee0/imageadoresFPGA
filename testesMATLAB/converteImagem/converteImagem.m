fa6d5ec86831ccdda165ef7d1cf6428b367a1f94
Gabriel Oliveira <gabriel.oliveira@engenharia.ufjf.br>
2019-11-20 21:39:35 -0300

    tentando corrigir a transformação

--- testesMATLAB/converteImagem/converteImagem.m
+++ testesMATLAB/converteImagem/converteImagem.m
@@ -13,11 +13,14 @@ imagemCinza = rgb2gray (imagemLida);
 alturaImagem    = size(imagemCinza,1);
 larguraImagem   = size(imagemCinza,2);
 
-for a = 1:alturaImagem;  
+for a = 1:alturaImagem;
     for l = 1:larguraImagem;
         
         contadorBinario = dec2bin(contador ,8);
-        valorPixel = uint16((imagemCinza(a, l)))*4;
+        pixel = imagemCinza(a, l);
+        pixelCinza = typecast(pixel, 'single');
+        valor = (pixelCinza/255)*1023;
+        valorPixel = uint16(valor);
         
         texto = '\t\t8''d%d:\toVAL <= 10''d%d;\n';
         fprintf(saida, texto, contador, valorPixel);
