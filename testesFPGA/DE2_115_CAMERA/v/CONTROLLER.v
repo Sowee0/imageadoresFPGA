ea0dbdbc7706ec659126685ead37bafec8e15f6f
Gabriel Oliveira <gabriel.oliveira@engenharia.ufjf.br>
2019-11-20 21:40:25 -0300

    Mudando inicializações e tentando reordenar o fim

--- testesFPGA/DE2_115_CAMERA/v/CONTROLLER.v
+++ testesFPGA/DE2_115_CAMERA/v/CONTROLLER.v
@@ -26,13 +26,13 @@ output   reg 			oStatusLed = 1'b0;
 `include "SAVE_params.h"
 
 //Variable Declaration
-reg [12:0]	Xcounter;
-reg [12:0]	Ycounter;
+reg [12:0]	Xcounter = 0;
+reg [12:0]	Ycounter = 0;
 
 
-reg [12:0] biggestX;
-reg [12:0] biggestY;
-reg [31:0] biggestCorr;
+reg [12:0] biggestX = 0;
+reg [12:0] biggestY = 0;
+reg [31:0] biggestCorr = 0;
 wire[31:0] currentCorr;
 
 reg [25:0] statusCounter = 26'b0; 
@@ -59,13 +59,13 @@ always @ (posedge iCLK) begin
 			oStatusLed = 1'b1;
 	
 
-	if(iFrameDone) begin
-		biggestX 	<= 12'b0;
-		biggestY 	<= 12'b0;
-		Xcounter 	<= 12'b0;
-		Ycounter 	<= 12'b0;
-		biggestCorr <= 32'b0;
-	end
+//	if(iFrameDone) begin
+//		biggestX 	<= 12'b0;
+//		biggestY 	<= 12'b0;
+//		Xcounter 	<= 12'b0;
+//		Ycounter 	<= 12'b0;
+//		biggestCorr <= 32'b0;
+//	end
 	
 	if(iCorrFinished) begin
 		if(currentCorr > biggestCorr) begin
@@ -75,19 +75,21 @@ always @ (posedge iCLK) begin
 		end
 	end
 	
-	if(iFrameDone && iCorrFinished && !finished) begin
-				
-		if(Xcounter < H_RES && Ycounter < V_RES)
+	if(iFrameDone && !iCorrFinished && !finished) begin
+		
+		if(Xcounter == H_RES2 && Ycounter == V_RES2)begin
+			finished = 1'b1;
+		end
+		else if(Xcounter < H_RES2 && Ycounter < V_RES2)begin
 			Xcounter <= Xcounter + 12'b1;
+		end
 
-		else if(Xcounter == H_RES && Ycounter < V_RES) begin
-			Xcounter <= 12'b0;
+		else if(Xcounter == H_RES2 && Ycounter < V_RES2) begin
 			Ycounter <= Ycounter + 12'b1;
-		end
-
-		else if(Xcounter == H_RES && Ycounter == V_RES) begin
-			finished <= 1'b1;
-		end
+			if(Ycounter != V_RES2)
+				Xcounter <= 12'b0;
+		end 
+		
 	end
 	
 end
