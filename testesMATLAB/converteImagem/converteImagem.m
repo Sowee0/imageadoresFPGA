b956ec30442ab1199253fd5db1c727a3f6dc9f52
Gabriel Oliveira <gabriel.oliveira@engenharia.ufjf.br>
2019-11-22 14:55:55 -0300

    Versão finalizada do conversor em matlab

--- testesMATLAB/converteImagem/converteImagem.m
+++ testesMATLAB/converteImagem/converteImagem.m
@@ -9,6 +9,7 @@ imagemLida = imread('pupila.bmp');
 saida = fopen("saida.txt",'w');
 
 imagemCinza = rgb2gray (imagemLida);
+doubleCinza = im2double (imagemCinza);
 
 alturaImagem    = size(imagemCinza,1);
 larguraImagem   = size(imagemCinza,2);
@@ -17,14 +18,12 @@ for a = 1:alturaImagem;
     for l = 1:larguraImagem;
         
         contadorBinario = dec2bin(contador ,8);
-        pixel = imagemCinza(a, l);
-        pixelCinza = typecast(pixel, 'single');
-        valor = (pixelCinza/255)*1023;
+        pixel = doubleCinza(a, l);
+        valor = pixel*1023;
         valorPixel = uint16(valor);
         
         texto = '\t\t8''d%d:\toVAL <= 10''d%d;\n';
         fprintf(saida, texto, contador, valorPixel);
-        
         
         contador = contador + 1;
     end
