56e734973b09b1c096aa0223b6b2d6a9316d3d33
Gabriel Oliveira <sowee00@gmail.com>
2020-04-03 20:11:08 -0300

    Modificando os parametros para ter uma imagem menor e pesquisa também

--- testesFPGA/DE2_115_CAMERA/v/SAVE_params.h
+++ testesFPGA/DE2_115_CAMERA/v/SAVE_params.h
@@ -1,11 +1,32 @@
+/* The frame is written on the screen following these timings in order B
+
+
+edit this later on
+
+
+*/
+
+
+
+//Horizontal parameters for the saved frame for a 640x480 total frame resolution the resolution
+//that follows here is just what will actually be saved
+
+//This parameter tells when on the timing counter to start reading the frame
 parameter H_START  	= 12'd144;
+
+//This parameter is the total size of the frame, which is counted after the H_START has finished
+parameter H_RES		= 12'd320; 
+
+//The same thing happens for the vertical timings
 parameter V_START  	= 12'd35;
-parameter H_RES		= 12'd640;
-parameter V_RES		= 12'd480;
+parameter V_RES		= 12'd240;
 
-parameter SEARCH_H_RES = 12'd255;
-parameter SEARCH_V_RES = 12'd255;
+//Parameters for the search frame and its middle position 
+parameter SEARCH_H_RES = 12'd127;
+parameter SEARCH_V_RES = 12'd127;
 
+
+//Test parameters
 parameter H_RES2		= 12'd20;
 parameter V_RES2		= 12'd20;
 
